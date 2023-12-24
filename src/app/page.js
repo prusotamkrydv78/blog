37e6862423b7925a6bfb2db4f968c2522fae27e7
Prusotam Yadav <prusotamyadav110@gmail.com>
2023-12-24 20:22:17 +0545

    video 7 is completed

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,10 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+ 
+import styles from "./page.module.css";
 
 export default function Home() {
   return (
     <main className={styles.main}>
-      
+      <h1>Welcome to Next js Learning</h1>
     </main>
-  )
+  );
 }
